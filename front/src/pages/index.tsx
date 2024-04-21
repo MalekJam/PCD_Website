5cf50f778af492ee3adec3e159f8c6f5db34255e
MalekJam <malek.jamoussi@hotmail.com>
2024-04-21 02:19:49 +0100

    Modifications

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -4,6 +4,25 @@ import type { FC } from "react";
 import Chart from "react-apexcharts";
 import NavbarSidebarLayout from "../layouts/navbar-sidebar";
 
+
+
+
+
+const ChartContainer : FC = function ()
+{
+  return (
+      <div className="flex flex-row gap-4">
+        <div className="w-1/2">
+          <SalesChart2/>
+        </div>
+        <div className="w-1/2">
+          <SalesChart/>
+        </div>
+      </div>
+  );
+};
+
+
 const DashboardPage: FC = function () {
   return (
     <NavbarSidebarLayout>
@@ -49,7 +68,7 @@ const SalesThisWeek: FC = function () {
           </svg>
         </div>
       </div>
-      <SalesChart />
+      <ChartContainer />
       <div className="mt-5 flex items-center justify-between border-t border-gray-200 pt-3 dark:border-gray-700 sm:pt-6">
         <Datepicker />
         <div className="shrink-0">
@@ -145,6 +164,142 @@ const SalesChart: FC = function () {
         "07 Feb",
           "08 Feb",
           "09 Feb",
+      ],
+      labels: {
+        style: {
+          colors: [labelColor],
+          fontSize: "14px",
+          fontWeight: 500,
+        },
+      },
+      axisBorder: {
+        color: borderColor,
+      },
+      axisTicks: {
+        color: borderColor,
+      },
+      crosshairs: {
+        show: true,
+        position: "back",
+        stroke: {
+          color: borderColor,
+          width: 1,
+          dashArray: 10,
+        },
+      },
+    },
+    yaxis: {
+      labels: {
+        style: {
+          colors: [labelColor],
+          fontSize: "14px",
+          fontWeight: 500,
+        },
+        formatter: function (value) {
+          return "$" + value;
+        },
+      },
+    },
+    legend: {
+      fontSize: "14px",
+      fontWeight: 500,
+      fontFamily: "Inter, sans-serif",
+      labels: {
+        colors: [labelColor],
+      },
+      itemMargin: {
+        horizontal: 10,
+      },
+    },
+    responsive: [
+      {
+        breakpoint: 1024,
+        options: {
+          xaxis: {
+            labels: {
+              show: false,
+            },
+          },
+        },
+      },
+    ],
+  };
+  const series = [
+    {
+      name: "Revenue",
+      data: [6356, 6218, 6156, 6526, 6356, 6256, 6056,200,10000],
+      color: "#1A56DB",
+    },
+  ];
+
+  return <Chart height={420} options={options} series={series} type="area" />;
+};
+const SalesChart2: FC = function () {
+  const { mode } = useTheme();
+  const isDarkTheme = mode === "dark";
+
+  const borderColor = isDarkTheme ? "#374151" : "#F3F4F6";
+  const labelColor = isDarkTheme ? "#93ACAF" : "#6B7280";
+  const opacityFrom = isDarkTheme ? 0 : 1;
+  const opacityTo = isDarkTheme ? 0 : 1;
+
+  const options: ApexCharts.ApexOptions = {
+    stroke: {
+      curve: "smooth",
+    },
+    chart: {
+      type: "area",
+      fontFamily: "Inter, sans-serif",
+      foreColor: labelColor,
+      toolbar: {
+        show: false,
+      },
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        opacityFrom,
+        opacityTo,
+        type: "vertical",
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    tooltip: {
+      style: {
+        fontSize: "14px",
+        fontFamily: "Inter, sans-serif",
+      },
+    },
+    grid: {
+      show: true,
+      borderColor: borderColor,
+      strokeDashArray: 1,
+      padding: {
+        left: 35,
+        bottom: 15,
+      },
+    },
+    markers: {
+      size: 5,
+      strokeColors: "#ffffff",
+      hover: {
+        size: undefined,
+        sizeOffset: 3,
+      },
+    },
+    xaxis: {
+      categories: [
+        "01 Feb",
+        "02 Feb",
+        "03 Feb",
+        "04 Feb",
+        "05 Feb",
+        "06 Feb",
+        "07 Feb",
+        "08 Feb",
+        "09 Feb",
       ],
       labels: {
         style: {
